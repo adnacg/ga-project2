1fc297204551901402d709d02dabc856372b6623
adna <adna@adna.com>
2018-07-11 23:48:43 +0800

    Change database library

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
+// const pg = require('pg');
 const pg = require('pg');
+const Pool = require('pg-pool')
 const url = require('url');
 
 // Initialise postgres client
 const dbUrl = process.env.DATABASE_URL;
 
+let configs;
+
 if( process.env.DATABASE_URL ){
     const params = url.parse(dbUrl);
     const auth = params.auth.split(':');
 
-    var configs = {
+    configs = {
         user: auth[0],
         password: auth[1],
         host: params.hostname,
@@ -17,16 +21,18 @@ if( process.env.DATABASE_URL ){
         ssl: true
     };
 
+    const pool2 = new Pool(config);
 } else {
-    const config = {
+    config = {
       user: 'elvera',
       host: '127.0.0.1',
       database: 'jamaway',
       port: 5432,
     };
+
+    const pool = new pg.Pool(config);
 }
 
-const pool = new pg.Pool(config);
 
 pool.on('error', function (err) {
   console.log('idle client error', err.message, err.stack);
